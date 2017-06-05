bdf18a9a6c1e80aecfa8c0eab192e22acd41a789
anubhav253 <anubhav253kumar@gmail.com>
2017-06-05 23:01:45 +0530

    Node and class is implmented

--- tree.cpp
+++ tree.cpp
@@ -2,6 +2,41 @@
 
 using namespace std;
 
+class treeNode{
+	private: 
+		int val;
+		treeNode* left;
+		treeNode* right;
+
+	
+	public:
+		treeNode(int n){
+			val=n;
+			left=NULL;
+			right=NULL;
+		}
+		void setVal(int v){
+			val=v;
+		}
+		int getVal(){
+			return val;
+		}
+		void setLeft(treeNode* l){
+			left=l;
+		}
+		treeNode* getLeft(){
+			return left;
+		}
+		void setRight(treeNode* r){
+			right=r;
+		}
+		treeNode* getRight(){
+			return right;
+		}
+};
+
+
+
 int main(){
 	cout<<"Hello World"<<endl;
 	return 0;
